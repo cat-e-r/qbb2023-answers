95d2a1522e5e9f6c9c65ff759161bf7110daef64
Catherine <croger65@jh.edu>
2023-09-07 17:38:52 -0400

    Add answer for excercise 3 remove troubleshooting text

--- day3-homework/mean_inflamation.py
+++ day3-homework/mean_inflamation.py
@@ -16,33 +16,5 @@ def my_mean(X, file_name):
         days_int.append(int(day))
     return sum(days_int)/len(days_int)
     
-        #patient_split[day]
-    #     patient_split = int(patient_split)
-    # print(patient_split)
-
-
-
-
-# def my_mean(X, file_name):
-#     #X is a row
-#     #file_name is a string
-#     file_name = sys.argv[1]
-#     data = open(file_name)
-#     inflammation = data.readlines()
 
 print(my_mean(40, "inflammation-01.csv"))
-
-
-
-    
-
-# numbers = []
-# for line in data:
-#     cleanline = line.rstrip("\n")
-#     patient_data = line.split(",")
-#     score = int(cleanline)
-#     numbers.append(score)
-
-#print(numbers)
-
-#print(my_mean(numbers))
