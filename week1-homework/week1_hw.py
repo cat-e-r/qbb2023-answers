f0fc05d88cfc4efd5249944dcf90dab31d31bf77
Catherine <croger65@jh.edu>
2023-09-15 13:54:42 -0400

    Add answers 1.1 and 1.2

---
+++ week1-homework/week1_hw.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+import scipy.stats as sps
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+
+# Exercise 1: Wrangle the data
+# Step 1.1
+
+# You’ll start by exploring the data in aau1043_dnm.csv. First, load this data into a pandas dataframe.
+
+df = pd.read_csv('aau1043_dnm.csv')
+#print(df)
+
+# Step 1.2
+
+# You first want to count the number of paternally and maternally 
+#inherited DNMs in each proband. Using this dataframe, create a 
+#dictionary where the keys are the proband IDs and the value 
+#associated with each key is a list of length 2, where the first 
+#element in the list is the number of maternally inherited DNMs 
+#and the second element in the list is the number of paternally 
+#inherited DNMs for that proband. You can ignore DNMs without 
+#a specified parent of origin.
+
+
+pat_dnms = df.loc[:,"Phase_combined"] == "father"
+pat_pro_id = df.loc[pat_dnms, "Proband_id"]
+mat_dnms = df.loc[:,"Phase_combined"] == "mother"
+mat_pro_id = df.loc[mat_dnms, "Proband_id"]
+
+#print (pat_pro_id)
+#print(mat_pro_id)
+
+my_dictionary = {}
+for i in df.loc[:, "Proband_id"]:
+    my_dictionary[i] = [0, 0]
+
+for pbid in mat_pro_id:
+    my_dictionary[pbid][0] += 1
+
+for pbid in pat_pro_id:
+    my_dictionary[pbid][1] += 1
+
+
+print(my_dictionary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
