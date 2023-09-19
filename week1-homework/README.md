a31a85608f256b340b62a6081469f8ab6696a9c1
Catherine <croger65@jh.edu>
2023-09-19 18:13:59 -0400

    Add remainder of responses

---
+++ week1-homework/README.md
@@ -0,0 +1,28 @@
+EXERCISE 2.2
+What is the “size” of this relationship? In your own words, what does this mean? Does this match what you observed in your plots in step 2.1?
+    As age increases by one year the mother passes on 0.3776 times as many de novo mutations. This matches what I observed in step 2.1 where with the mother's age de novo mutations increased, but not super quickly.
+
+
+Is this relationship significant? How do you know?
+    Yes, because the p-value is pretty close to 0 meaning we can reject the null hypothesis that there is no relationship between the mother's age and the number of de novo mutations.
+
+EXERCISE 2.3
+What is the “size” of this relationship? In your own words, what does this mean? Does this match what you observed in your plots in step 2.1?
+    As age increases by one year the father passes on 1.35 times as many de novo mutations. This matches what I observed in step 2.1 where with the father's age de novo mutations increase more quickly than in the case of the mother's age.
+
+
+Is this relationship significant? How do you know?
+    Yes, because the p-value is pretty close to 0 meaning we can reject the null hypothesis that there is no relationship between the father's age and the number of de novo mutations.
+
+EXERCISE 2.4
+Using your results from step 2.3, predict the number of paternal DNMs for a proband with a father who was 50.5 years old at the proband’s time of birth. Record your answer and your work (i.e. how you got to that answer) in your README.md.
+
+    y = (1.3538)(50.5) + 10.3263 = 78.6932
+
+EXERCISE 2.6
+What statistical test did you choose? Why?
+    I used a ttest to compare the means of each group, maternal and paternal, to see how different they are.
+
+Was your test result statistically significant? Interpret your result as it relates to the number of paternally and maternally inherited DNMs.
+    Yes, the result is statistically significant, because the p-value is very close to 0. This means that the probands experienced significantly fewer paternal de novo mutations.
+
--- week1-homework/week1_hw.py
+++ week1-homework/week1_hw.py
@@ -8,7 +8,7 @@ import scipy.stats as sps
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
 
-# Exercise 1: Wrangle the data
+# EXERCISE 1: Wrangle the data
 # Step 1.1
 
 # You’ll start by exploring the data in aau1043_dnm.csv. First, load this data into a pandas dataframe.
@@ -46,24 +46,87 @@ for pbid in mat_pro_id:
 for pbid in pat_pro_id:
     my_dictionary[pbid][1] += 1
 
-
-#print(my_dictionary)
+# Step 1.3
 
 my_dictionary_DF = pd.DataFrame.from_dict(my_dictionary, orient = 'index', columns = ['maternal_dnm', 'paternal_dnm'])
 
+# Step 1.4
+
 par_data = pd.read_csv('aau1043_parental_age.csv', index_col = 'Proband_id')
+
+# Step 1.5
 
 total_data = pd.concat([my_dictionary_DF, par_data], axis = 1, join = 'inner')
 
 print(total_data)
 
+#________________________________________________________________________________________________
 
+# EXERCISE 2: Fit and interpret linear regression models with Python
 
+# Step 2.1
 
+x_f = total_data.loc[:, "Mother_age"]
+y_f = total_data.loc[:, "maternal_dnm"]
 
+fig, ax = plt.subplots()
+ax.set_title( "Maternal de novo Mutations \n with Age")
+ax.scatter(x_f, y_f, c = 'pink')
+#ax.hist(x)
+ax.set_xlabel("maternal age")
+ax.set_ylabel("de novo mutations")
+plt.tight_layout()
+figure = fig.savefig( "ex2_a.png" )
+#plt.show()
 
+x_m = total_data.loc[:, "Father_age"]
+y_m = total_data.loc[:, "paternal_dnm"]
 
+fig, ax = plt.subplots()
+ax.set_title( "Paternal de novo Mutations \n with Age")
+ax.scatter(x_m, y_m, c = 'cyan')
+#ax.hist(x)
+ax.set_xlabel("paternal age")
+ax.set_ylabel("de novo mutations")
+plt.tight_layout()
+figure = fig.savefig( "ex2_b.png" )
+#plt.show()
 
+# Step 2.2
+
+mat_ols = smf.ols(formula = 'maternal_dnm ~ 1 + Mother_age' , data = total_data)
+mat_ols_fit = mat_ols.fit()
+print(mat_ols_fit.params)
+print(mat_ols_fit.pvalues)
+print(mat_ols_fit.summary())
+#see README.md for analysis
+
+# Step 2.3
+
+pat_ols = smf.ols(formula = 'paternal_dnm ~ 1 + Father_age' , data = total_data)
+pat_ols_fit = pat_ols.fit()
+print(pat_ols_fit.summary())
+#see README.md for analysis
+
+# Step 2.4
+#see README.md for answer
+
+# Step 2.5
+
+fig, ax = plt.subplots()
+ax.set_title( "Paternal vs. Maternal\nde novo Mutations")
+ax.hist(y_m, label = 'maternal', alpha = 0.5, color = 'pink')
+ax.hist(y_f, label = 'paternal', alpha = 0.4, color = 'cyan')
+ax.set_xlabel("# of de novo mutations")
+ax.set_ylabel("ocurrances")
+ax.legend()
+plt.tight_layout()
+figure = fig.savefig( "ex2_c.png" )
+plt.show()
+
+# Step 2.6
+print(sps.ttest_ind(y_m, y_f))
+#see analysis in README.md
 
 
 
