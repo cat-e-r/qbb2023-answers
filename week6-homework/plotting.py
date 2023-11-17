1d4805005780176e23d04d1f4a05d4e85299cbe1
Catherine <croger65@jh.edu>
2023-11-17 13:49:46 -0500

    changed my manhattan plots

--- week6-homework/plotting.py
+++ week6-homework/plotting.py
@@ -36,73 +36,77 @@ fig2.savefig("Allele_freq_plot.png")
 gwas_G = pd.read_csv('GS451_gwas_results.assoc.linear', delim_whitespace =True)
 gwas_C = pd.read_csv('CB1908_gwas_results.assoc.linear', delim_whitespace =True)
 
+gwas_G = gwas_G.loc[gwas_G['TEST'] == 'ADD', :]
+gwas_C = gwas_C.loc[gwas_C['TEST'] == 'ADD', :]
+
 gwas_G['-log10_P'] = -(np.log10(gwas_G['P']))
 gwas_C['-log10_P'] = -(np.log10(gwas_C['P']))
 
 
-# fig3, axs = plt.subplots(2,1, figsize = [13, 8])
+fig3, axs = plt.subplots(2,1, figsize = [13, 8])
 
-# axs[0].scatter(gwas_G['BP'], gwas_G['-log10_P'], c=gwas_G['P']< 1e-5, cmap = 'Dark2')
-# axs[0].axhline(-np.log10(1e-5), color = 'black', linestyle='--', label='Threshold (1e-5)')
-# #axs[0].set_yscale('log')
-# axs[0].set_xlabel('SNP Loc')
-# axs[0].set_ylabel('-log10(P-val)')
-# axs[0].set_title('GS451_IC50')
-# axs[0].grid(True, linestyle = '--', alpha = 0.7)
-# axs[0].set_ylim(0,10)
-# axs[0].legend()
+axs[0].scatter(gwas_G.index, gwas_G['-log10_P'], c=gwas_G['P']< 1e-5, cmap = 'Dark2')
+axs[0].axhline(-np.log10(1e-5), color = 'black', linestyle='--', label='Threshold (1e-5)')
+#axs[0].set_yscale('log')
+axs[0].set_xlabel('SNP Loc')
+axs[0].set_ylabel('-log10(P-val)')
+axs[0].set_title('GS451_IC50')
+axs[0].grid(True, linestyle = '--', alpha = 0.7)
+axs[0].set_ylim(0,10)
+axs[0].legend()
 
-# axs[1].scatter(gwas_C['BP'], gwas_C['-log10_P'], c=gwas_C['P']< 1e-5 , cmap = 'Set2')
-# axs[1].axhline(-np.log10(1e-5), color = 'black', linestyle='--', label='Threshold (1e-5)')
-# #axs[1].set_yscale('log')
-# axs[1].set_xlabel('SNP Loc')
-# axs[1].set_ylabel('-log10(P-val)')
-# axs[1].set_title('CB1908_IC50')
-# axs[1].grid(True, linestyle = '--', alpha = 0.7)
-# axs[1].set_ylim(0,10)
-# axs[1].legend()
-# plt.tight_layout()
-# fig3.savefig("Manhattans.png")
+#axs[1].scatter(gwas_C['BP'], gwas_C['-log10_P'], c=gwas_C['P']< 1e-5 , cmap = 'Set2')
+axs[1].scatter(gwas_C.index, gwas_C['-log10_P'], c=gwas_C['P']< 1e-5 , cmap = 'Set2')
+axs[1].axhline(-np.log10(1e-5), color = 'black', linestyle='--', label='Threshold (1e-5)')
+#axs[1].set_yscale('log')
+axs[1].set_xlabel('SNP Loc')
+axs[1].set_ylabel('-log10(P-val)')
+axs[1].set_title('CB1908_IC50')
+axs[1].grid(True, linestyle = '--', alpha = 0.7)
+axs[1].set_ylim(0,10)
+axs[1].legend()
+plt.tight_layout()
+fig3.savefig("Manhattans.png")
 
-fig4, ax4 = plt.subplots()
-maxP = np.max(gwas_G['-log10_P'])
-rowID = gwas_G[gwas_G['-log10_P'] == maxP]
-snpID = rowID.loc[:,'SNP'].values[0]
+# fig4, ax4 = plt.subplots()
+# maxP = np.max(gwas_G['-log10_P'])
+# rowID = gwas_G[gwas_G['-log10_P'] == maxP]
+# snpID = rowID.loc[:,'SNP'].values[0]
 
-maxP2 = np.max(gwas_C['-log10_P'])
-rowID2 = gwas_C[gwas_C['-log10_P'] == maxP2]
-snpID2 = rowID2.loc[:,'SNP'].values[0]
+# maxP2 = np.max(gwas_C['-log10_P'])
+# rowID2 = gwas_C[gwas_C['-log10_P'] == maxP2]
+# snpID2 = rowID2.loc[:,'SNP'].values[0]
 
-GT_vcf = pd.read_csv('genotypes.vcf', delimiter = '\t', skiprows = 27)
-GOI = GT_vcf[GT_vcf['ID']==snpID]
+# GT_vcf = pd.read_csv('genotypes.vcf', delimiter = '\t', skiprows = 27)
+# GOI = GT_vcf[GT_vcf['ID']==snpID]
 
-POI = pd.read_csv('GS451_IC50.txt', delim_whitespace = True)
+# POI = pd.read_csv('GS451_IC50.txt', delim_whitespace = True)
 
-wt = []
-het = []
-hom = []
+# wt = []
+# het = []
+# hom = []
 
-plt_me = [wt, het, hom]
+# plt_me = [wt, het, hom]
 
-for i in range(len(GOI.values[0])):
-    if GOI.values[0][i]== '0/0':
-       wt.append(POI.iloc[i-7,2])
-    if GOI.values[0][i]== '0/1':
-       het.append(POI.iloc[i-7,2])
-    if GOI.values[0][i]== '1/1':
-       hom.append(POI.iloc[i-7,2])
+# for i in range(len(GOI.values[0])):
+#     if GOI.values[0][i]== '0/0':
+#        wt.append(POI.iloc[i-7,2])
+#     if GOI.values[0][i]== '0/1':
+#        het.append(POI.iloc[i-7,2])
+#     if GOI.values[0][i]== '1/1':
+#        hom.append(POI.iloc[i-7,2])
 
-for i in range(len(plt_me)):
-    plt_me[i] = [x for x in plt_me[i] if str(x) != 'nan']
+# for i in range(len(plt_me)):
+#     plt_me[i] = [x for x in plt_me[i] if str(x) != 'nan']
 
-ax4.boxplot(plt_me)
-ax4.set_title( "Genotypes and their phenotypes")
-ax4.set_xlabel('Genoype')
-ax4.set_ylabel('Phenotype')
-plt.savefig("Boxplot.png")
+# ax4.boxplot(plt_me)
+# ax4.set_title( "Genotypes and their phenotypes")
+# ax4.set_xlabel('Genoype')
+# ax4.set_ylabel('Phenotype')
+# plt.savefig("Boxplot.png")
 
-print(snpID)
-print(snpID2)
+# print(snpID)
+# print(snpID2)
 
 
 
